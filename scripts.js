b2f5810dd9644211d394e10a663e5ea50b86c05f
elias-hezron <eliashezron23.com>
2021-01-12 08:12:21 +0300

    complete

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
+const openMenu = select('.fa-bars')
+const closeMenu = select('.fa-times')
+const navList = select('.nav-list')
 
+function select(el){
+    return document.querySelector(el)
+}
+
+openMenu.addEventListener('click',()=>{
+    openMenu.classList.toggle('open', true)
+    closeMenu.classList.toggle('open',true)
+    navList.classList.toggle('active', true)
+})
+closeMenu.addEventListener('click', ()=>{
+    closeMenu.classList.toggle('open', false)
+    openMenu.classList.toggle('open', false)
+    navList.classList.remove('active', false)
+
+})
+navList.addEventListener
+
+AOS.init({
+    easing:'ease',
+    duration:1000
+})
